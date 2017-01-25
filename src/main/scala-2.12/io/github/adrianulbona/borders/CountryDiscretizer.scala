35da43509927a4ba9b53a40fb7f0cb28b609b6cb
Adrian Bona <adrianulbona@gmail.com>
2017-01-25 07:40:20 +0200

    Remove debugging code

--- src/main/scala-2.12/io/github/adrianulbona/borders/CountryDiscretizer.scala
+++ src/main/scala-2.12/io/github/adrianulbona/borders/CountryDiscretizer.scala
@@ -6,10 +6,8 @@ import java.lang.System.currentTimeMillis
 import ch.hsr.geohash.GeoHash
 import com.github.tototoshi.csv.CSVReader
 import com.github.tototoshi.csv.CSVWriter.open
-import com.vividsolutions.jts.geom.{GeometryFactory, Polygon}
-import com.vividsolutions.jts.io.{WKTReader, WKTWriter}
+import com.vividsolutions.jts.io.WKTReader
 import io.github.adrianulbona.jts.discretizer.DiscretizerFactoryImpl
-import io.github.adrianulbona.jts.discretizer.util.{GeoHash2Geometry, WGS84Point2Coordinate}
 
 import scala.collection.JavaConverters._
 
@@ -22,25 +20,10 @@ object CountryDiscretizer {
     def toList: List[String] = List(continent, name, fipsCode, isoCode)
   }
 
-  case class CountryBorder(id: Id, wkt: String)
+  case class Border(id: Id, wkt: String)
 
-  case class CountryDiscretization(country: Id, geoHashes: Set[GeoHash]) {
-    def toList: List[String] = {
-      country.toList :+ geoHashes.map(_.toBase32).mkString(",")
-    }
-
-    def toListWKT: List[String] = {
-      country.toList :+ geoHashes2wkt(geoHashes)
-    }
-
-    def geoHashes2wkt(geoHashes: Set[GeoHash]): String = {
-      val geoHash2Geometry = new GeoHash2Geometry(new WGS84Point2Coordinate)
-      val factory = new GeometryFactory()
-      val polygons = geoHashes
-        .map(geoHash => geoHash2Geometry.apply(geoHash, factory).asInstanceOf[Polygon])
-        .toArray
-      new WKTWriter().write(factory.createMultiPolygon(polygons))
-    }
+  case class Surface(country: Id, geoHashes: Set[GeoHash]) {
+    def toList: List[String] = country.toList :+ geoHashes.map(_.toBase32).mkString(",")
   }
 
   def main(args: Array[String]): Unit = {
@@ -48,21 +31,20 @@ object CountryDiscretizer {
     write(read()
       .par
       .map(discretize(precision)(_))
-      .toList, precision)
+      .toList)
   }
 
-
-  def read(): List[CountryBorder] = {
+  def read(): List[Border] = {
     val reader = CSVReader.open(new File("data/borders_wkt.csv"))
     try {
       reader.all().map({
-        case List(continent, name, fips, iso, wkt) => CountryBorder(Id(continent, name, fips, iso), wkt)
+        case List(continent, name, fips, iso, wkt) => Border(Id(continent, name, fips, iso), wkt)
       })
     }
     finally reader.close()
   }
 
-  def discretize(precision: Int)(border: CountryBorder): CountryDiscretization = {
+  def discretize(precision: Int)(border: Border): Surface = {
     val geometry = new WKTReader().read(border.wkt)
     val refTime = currentTimeMillis()
     val geoHashes = new DiscretizerFactoryImpl()
@@ -70,20 +52,13 @@ object CountryDiscretizer {
       .apply(geometry, precision).asScala.toSet
     val duration = currentTimeMillis() - refTime
     println(s"Discretized ${border.id.name} with ${geoHashes.size} geoHashes in $duration ms.")
-    CountryDiscretization(border.id, geoHashes)
+    Surface(border.id, geoHashes)
   }
 
-  def write(discretizations: List[CountryDiscretization], precision: Int): Unit = {
-    val writerGeoHashes = open(new File(s"data/countries_discretized_$precision.csv"))
-    try {
+  def write(discretizations: List[Surface]): Unit = {
+    val writerGeoHashes = open(new File(s"data/countries_discretized.csv"))
+    try
       writerGeoHashes.writeAll(discretizations.map(_.toList))
-    }
     finally writerGeoHashes.close()
-
-    val writerWKT = open(new File(s"data/countries_discretized_${precision}_wkt.csv"))
-    try {
-      writerWKT.writeAll(discretizations.map(_.toListWKT))
-    }
-    finally writerWKT.close()
   }
 }
